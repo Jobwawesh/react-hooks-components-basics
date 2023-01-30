d55049ddba4874caf272b83512cddb1c8e314636
Jobwawesh <jobnjunge15@gmail.com>
2023-01-30 21:53:22 +0300

    Assignment completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from "react";
-import Article from "./Article";
-import Comment from "./Comment";
-
-function App() {
+function NavBar() {
   return (
-    <div>
-      <Article />
-      <Comment />
+    <nav>
+      <a href="#home">I'm a link!</a>
+    </nav>
+  );
+}
+function Home() {
+  return (
+    <div id="home">
+      <h1>Home</h1>
     </div>
   );
 }
 
+function About(){
+  return(
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  )
+}
+
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+    </div>
+  );
+}
 export default App;
